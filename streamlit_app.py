28500561b284869d951c3c329b23b29bf7a800f2
Kellee Springer <kelleebellspringer@gmail.com>
2024-12-08 14:46:59 +0000

    requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import time # for simulating a real-time data, time loop                    
-import pandas as pd # read csv, df manipulation                 
-import plotly.express as px # interactive charts              
+import pandas as pd # read csv, df manipulation                               
 import streamlit as st # data web application development
 
 st.set_page_config(
@@ -42,23 +41,24 @@ df
 data_filter = st.selectbox("Select Data", ("Total Nonfarm Employment", "Unemployment Rate", "Civilian Unemployment", "Civilian Labor Force"))
 df = df[df['series_id'] == data_filter]
 
-fig_col1, fig_col2, fig_col3, fig_col4 = st.columns(4)
-with fig_col1:
-    st.markdown("### Total Nonfarm Employment")
-    fig = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
-    st.plotly_chart(fig, use_container_width=True)
+#fig_col1, fig_col2, fig_col3, fig_col4 = st.columns(4)
+#with fig_col1:
+    #import plotly.express as px
+    #st.markdown("### Total Nonfarm Employment")
+    #fig = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    #st.plotly_chart(fig, use_container_width=True)
 
-with fig_col2:
-    st.markdown("### Unemployment Rate")
-    fig2 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
-    st.plotly_chart(fig2, use_container_width=True)
+#with fig_col2:
+    #st.markdown("### Unemployment Rate")
+    #fig2 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    #st.plotly_chart(fig2, use_container_width=True)
 
-with fig_col3:
-    st.markdown("### Civilian Unemployment")
-    fig3 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
-    st.plotly_chart(fig3, use_container_width=True)
+#with fig_col3:
+    #st.markdown("### Civilian Unemployment")
+    #fig3 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    #st.plotly_chart(fig3, use_container_width=True)
 
-with fig_col4:
-    st.markdown("### Civilian Labor Force")
-    fig4 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
-    st.plotly_chart(fig4, use_container_width=True)
+#with fig_col4:
+    #st.markdown("### Civilian Labor Force")
+    #fig4 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    #st.plotly_chart(fig4, use_container_width=True)
