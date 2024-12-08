0009674f6c80abb3c52c5e27056a4905c31ae0b4
Kellee Springer <kelleebellspringer@gmail.com>
2024-12-08 05:27:50 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,21 @@
-import streamlit as st
+import time # for simulating a real-time data, time loop                    
+import pandas as pd # read csv, df manipulation                 
+import plotly.express as px # interactive charts              
+import streamlit as st # data web application development
 
+st.set_page_config(
+    page_title="US Bureau of Labor Statistics Dashboard",
+    page_icon=":tophat:",
+    layout="wide",
+)
 st.title("US Bureau of Labor Statistics Dashboard")
 st.write("Lets look at Total Nonfarm Employment, Unemployment Rate, Civilian Unemployment, and Civilian Labor Force. Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
-
-import requests
-import json
-import pandas as pd
 
 headers = {'Content-type': 'application/json'}
 data = json.dumps({"seriesid": ['CEU0000000001','LNS14000000', 'LNS11000000', 'LNS13000000'],"startyear":"2023", "endyear":"2024"})
 p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
 json_data = json.loads(p.text)
 
-print(json_data)
 
 def process_bls_data(json_data):
     data_list = []
@@ -33,8 +36,29 @@ def process_bls_data(json_data):
     df = pd.DataFrame(data_list)
     return df
 
-
 df = process_bls_data(json_data)
-
 df
 
+data_filter = st.selectbox("Select Data", ("Total Nonfarm Employment", "Unemployment Rate", "Civilian Unemployment", "Civilian Labor Force"))
+df = df[df['series_id'] == data_filter]
+
+fig_col1, fig_col2, fig_col3, fig_col4 = st.columns(4)
+with fig_col1:
+    st.markdown("### Total Nonfarm Employment")
+    fig = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    st.plotly_chart(fig, use_container_width=True)
+
+with fig_col2:
+    st.markdown("### Unemployment Rate")
+    fig2 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    st.plotly_chart(fig2, use_container_width=True)
+
+with fig_col3:
+    st.markdown("### Civilian Unemployment")
+    fig3 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    st.plotly_chart(fig3, use_container_width=True)
+
+with fig_col4:
+    st.markdown("### Civilian Labor Force")
+    fig4 = px.line(df, x='year', y='value', title=f'{data_filter} Over Time')
+    st.plotly_chart(fig4, use_container_width=True)
